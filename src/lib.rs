8a464274265263f9de66ef6fde36d51187f1a3c5
est31 <MTest31@outlook.com>
2022-01-14 14:39:24 +0100

    Remove redundant extern crate statements

--- src/lib.rs
+++ src/lib.rs
@@ -19,15 +19,9 @@ Ogg container decoder and encoder
 The most interesting structures for in this
 mod are `PacketReader` and `PacketWriter`.
 */
-
-extern crate byteorder;
-#[cfg(feature = "async")]
-extern crate tokio_io;
 #[cfg(feature = "async")]
 #[macro_use]
 extern crate futures;
-#[cfg(feature = "async")]
-extern crate bytes;
 
 #[cfg(test)]
 mod test;
